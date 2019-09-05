20ef4bfdfedd87a7270d1aa72e5bce20b5f11d3f
W. X. Chan <w.x.chan1986@gmail.com>
2019-09-05 17:45:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 setup(
-  name = 'medImgProc',         # How you named your package folder (MyLib)
-  packages = ['medImgProc'],   # Chose the same as "name"
-  version = '1.0.1',      # Start with a small number and increase it with every change you make
+  name = 'motionSegmentation',         # How you named your package folder (MyLib)
+  packages = ['motionSegmentation'],   # Chose the same as "name"
+  version = '1.1.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Medical Image Processing module for viewing and editing.',   # Give a short description about your library
   author = 'Wei Xuan Chan',                   # Type in your name
   author_email = 'w.x.chan1986@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/WeiXuanChan/medImgProc',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/WeiXuanChan/medImgProc/archive/v1.0.0.tar.gz',    # I explain this later on
-  keywords = ['medical', 'image'],   # Keywords that define your package best
-  install_requires=['numpy','matplotlib','imageio','scipy','multiprocessing','pickle','inspect','trimesh','SimpleITK','re','pywt'],
+  url = 'https://github.com/WeiXuanChan/motionSegmentation',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/WeiXuanChan/motionSegmentation/archive/v1.1.0.tar.gz',    # I explain this later on
+  keywords = ['explicit', 'motion'],   # Keywords that define your package best
+  install_requires=['numpy','autoD','scipy','multiprocessing','pickle','time','trimesh','re'],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package    
     'Intended Audience :: Developers',      # Define that your audience are developers
