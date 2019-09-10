bcb2a6051999955be9038fe98d1ac23730bd02ef
W. X. Chan <w.x.chan1986@gmail.com>
2019-09-10 18:11:38 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
   url = 'https://github.com/WeiXuanChan/motionSegmentation',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/WeiXuanChan/motionSegmentation/archive/v1.1.0.tar.gz',    # I explain this later on
   keywords = ['explicit', 'motion'],   # Keywords that define your package best
-  install_requires=['numpy','autoD','scipy','multiprocessing','pickle','time','trimesh','re'],
+  install_requires=['numpy','autoD','scipy','trimesh'],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package    
     'Intended Audience :: Developers',      # Define that your audience are developers
