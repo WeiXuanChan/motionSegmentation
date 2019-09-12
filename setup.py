dcfa7b315a5ffd0100c9b4c8940ace90883c2eda
W. X. Chan <w.x.chan1986@gmail.com>
2019-09-12 12:58:16 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 setup(
   name = 'motionSegmentation',         # How you named your package folder (MyLib)
   packages = ['motionSegmentation'],   # Chose the same as "name"
-  version = '1.1.0',      # Start with a small number and increase it with every change you make
+  version = '1.2.0',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Explicit motion tracking.',   # Give a short description about your library
+  description = 'Explicit spatio-temporal regularization of motion tracking.',   # Give a short description about your library
   author = 'Wei Xuan Chan',                   # Type in your name
   author_email = 'w.x.chan1986@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/WeiXuanChan/motionSegmentation',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/WeiXuanChan/motionSegmentation/archive/v1.1.0.tar.gz',    # I explain this later on
-  keywords = ['explicit', 'motion'],   # Keywords that define your package best
+  download_url = 'https://github.com/WeiXuanChan/motionSegmentation/archive/v1.2.0.tar.gz',    # I explain this later on
+  keywords = ['explicit', 'motion', 'regularization'],   # Keywords that define your package best
   install_requires=['numpy','autoD','scipy','trimesh'],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package    
