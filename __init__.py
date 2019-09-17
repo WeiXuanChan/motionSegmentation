118e8a54c07d3f305a42f0af8ac57edbcc2aa08e
W. X. Chan <w.x.chan1986@gmail.com>
2019-09-17 15:42:41 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,6 +15,9 @@ Author: w.x.chan@gmail.com         12SEP2019           - v2.0.0
 Author: w.x.chan@gmail.com         12SEP2019           - v2.1.0
 						-bfSolver version 2.1.0
 						-BsplineFourier version 2.0.0
+Author: w.x.chan@gmail.com         12SEP2019           - v2.2.0
+						-bfSolver version 2.1.0
+						-BsplineFourier version 2.2.0
 
 Requirements:
     autoD
