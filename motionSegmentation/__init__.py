3ba2f1f5f72742bdfd7f91a4a2e9fd2f55f9114c
W. X. Chan <w.x.chan1986@gmail.com>
2019-09-26 17:27:20 +0800

    Update __init__.py

--- motionSegmentation/__init__.py
+++ motionSegmentation/__init__.py
@@ -1,4 +1,27 @@
 '''
+###############################################################################
+MIT License
+
+Copyright (c) 2019 W. X. Chan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+###############################################################################
 File: __init__.py
 Description: load all class for bfmotionsolver
              Contains linker to main classes
@@ -27,6 +50,7 @@ Author: jorry.zhengyu@gmail.com    26SEP2019           - v2.2.5
 Author: w.x.chan@gmail.com    26SEP2019                - v2.2.6
 						-bfSolver version 2.2.4
 						-BsplineFourier version 2.2.6
+						- do import *
 Requirements:
     autoD
     numpy
@@ -46,10 +70,6 @@ import os
 import sys
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)))
 
-import bfSolver as solver
-import BsplineFourier as bsfourier
+from BsplineFourier import *
+from bfSolver import *
 
-#linker to classes
-BFSolver=solver.bfSolver
-BsplineFourier=bsfourier.BsplineFourier
-Bspline=bsfourier.Bspline
