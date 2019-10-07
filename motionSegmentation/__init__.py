59cdb902057e0a04969536b97b44073912e86640
W. X. Chan <w.x.chan1986@gmail.com>
2019-10-07 13:24:04 +0800

    Update __init__.py

--- motionSegmentation/__init__.py
+++ motionSegmentation/__init__.py
@@ -54,9 +54,9 @@ Author: w.x.chan@gmail.com    26SEP2019                - v2.2.6
 Author: w.x.chan@gmail.com    07OCT2019                - v2.2.7
 						-bfSolver version 2.2.4
 						-BsplineFourier version 2.2.7
-Author: w.x.chan@gmail.com    07OCT2019                - v2.3.2
+Author: w.x.chan@gmail.com    07OCT2019                - v2.3.3
 						-bfSolver version 2.2.4
-						-BsplineFourier version 2.3.2
+						-BsplineFourier version 2.3.3
 Requirements:
     autoD
     numpy
@@ -69,7 +69,7 @@ Known Bug:
     HSV color format not supported
 All rights reserved.
 '''
-_version='2.3.2'
+_version='2.3.3'
 print('motionSegmentation version',_version)
 
 import os
