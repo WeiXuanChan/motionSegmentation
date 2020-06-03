666f2e0c16fe931e6158a472c86918f5445f5d77
JorryZ <45359771+JorryZ@users.noreply.github.com>
2020-06-03 20:25:08 +0800

    Update __init__.py

--- motionSegmentation/__init__.py
+++ motionSegmentation/__init__.py
@@ -110,6 +110,12 @@ Author: w.x.chan@gmail.com    06MAR2020                - v2.7.9
 						-BsplineFourier version 2.6.2
 						-motionCorrect version 2.7.8
 						-segment verion 2.7.9
+Author: jorry.zhengyu@gmail.com    03June2020                - v2.7.10
+						-bfSolver version 2.7.10
+						-BsplineFourier version 2.6.2
+						-motionCorrect version 2.7.8
+						-segment verion 2.7.9
+
 Requirements:
     autoD
     numpy
@@ -117,12 +123,13 @@ Requirements:
     scipy
     BsplineFourier
     pickle (optional)
+    nfft
 
 Known Bug:
     HSV color format not supported
 All rights reserved.
 '''
-_version='2.7.9'
+_version='2.7.10'
 import logging
 logger = logging.getLogger('motionSegmentation v'+_version)
 logger.info('motionSegmentation version '+_version)
